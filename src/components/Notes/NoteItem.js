849dd1cc954012db8e4313a540efbc611483e2db
Sinchan Maitra <86192575+abindent@users.noreply.github.com>
2022-03-05 19:51:54 +0530

    Update NoteItem.js

--- src/components/Notes/NoteItem.js
+++ src/components/Notes/NoteItem.js
@@ -6,6 +6,10 @@ const NoteItem = (props) => {
     const context = useContext(noteContext)
     const {deleteNote} = context
     const { note, updateNote } = props;
+    function createMarkup(c) {
+      return {__html: c};
+    }
+
      return (
         <div className="col-md-4">
             <Card style={{ width: '18rem' }} className="mb-2" >
@@ -13,7 +17,7 @@ const NoteItem = (props) => {
                         <Card.Title className="d-flex align-items-center">{note.title}<i className="bi bi-trash mx-2" onClick={()=>{deleteNote(note._id); props.showAlert("Note Deleted Successfully", "success")}}></i>
                         <i className="bi bi-pencil-square mx-2" onClick={()=>{updateNote(note); props.showAlert("Note Updated Successfully", "success");}}></i></Card.Title>
                         <Card.Subtitle className="mb-2 text-muted">Tags: {note.tag}</Card.Subtitle>
-                        <Card.Text><b>Body:</b> <br/>{note.description}</Card.Text>
+                        {note && <Card.Text dangerouslySetInnerHTML={createMarkup(note.description)}></Card.Text>}
                     </Card.Body>
             </Card>
         </div>
